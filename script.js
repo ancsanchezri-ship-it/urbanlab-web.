28c28b3544f9c262c0e241ed7318c8b7d945cfa5
ancsanchezri-ship-it <ancsanchezri@unal.edu.co>
2025-10-24 16:47:36 -0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function sendEmail(e){
-  e.preventDefault();
-  const form = e.target;
-  const name = form[0].value;
-  const email = form[1].value;
-  const message = form[2].value;
-  const subject = encodeURIComponent(`Contacto desde Urban+Lab: ${name}`);
-  const body = encodeURIComponent(`Nombre: ${name
